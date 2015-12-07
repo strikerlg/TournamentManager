f21bdb25db36bb5c96077a5136a2d8912ee414e9
Daniel Hauser <daniel.hauser@liwest.at>
2015-12-07 15:33:16 +0100

    SCHEISS XAMPP FÜR MAC

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+set_include_path("../lib" . PATH_SEPARATOR . "./");
+require_once "Twig/Autoloader.php";
+Twig_Autoloader::register();
 
+$db = $GLOBALS["DB"] = new Database();
+$db->connect();
