19dc94ba0c894d641927b58cd5108b035b69e50d
Daniel Hauser <daniel.hauser@liwest.at>
2015-12-07 15:44:33 +0100

    ENDLICH GEHT DER DRECKSSCHEISS

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 set_include_path("../lib" . PATH_SEPARATOR . "./");
 require_once "Twig/Autoloader.php";
 Twig_Autoloader::register();
