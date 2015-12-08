8358aa88a3bbd32207321f0fa866ea570b3212ae
Daniel Hauser <daniel.hauser@liwest.at>
2015-12-08 17:20:38 +0100

    Core PHP Logik implementiert
    - Twig wird nun für das Templating verwendet
    - Kint wird nun als var_dump Ersatz verwendet
    - Routing umgesetzt
    - Saubere OOP Umsetzung der Core Funktionalität
    - Cleanup von index.php
    - Templates für Twig angepasst
    - 404-Seite Prototyp umgesetzt

--- index.php
+++ index.php
@@ -1,32 +1,22 @@
 <?php
+
 set_include_path("lib" . PATH_SEPARATOR . "src");
 
 require_once "Kint/Kint.class.php";
-require_once "Twig/Autoloader.php";
-require_once "Repository/Database.php";
 
-//$DEV_MODE = "release";
-$DEV_MODE = "debug";
+//define("DEV_MODE", "release");
+define("DEV_MODE", "debug");
 Kint::enabled(false);
 
-if ($DEV_MODE === "debug")
+if (DEV_MODE === "debug")
 {
   ini_set('display_errors', 'On');
   error_reporting(E_ALL);
   Kint::enabled(true);
 }
 
-Twig_Autoloader::register();
+require_once "TManager.php";
 
-$db = $GLOBALS["DB"] = new Database();
-$db->connect();
-
-echo "<p>Simple 'describe MatchInfo' Query:<br>";
-d($db->query("describe MatchInfo"));
-
-echo "<p>Select-Query mit Argumenten, sicher gegen SQL-Injection<br>";
-d($db->query("select * from Admin where id = ? and username = ?", array(sqlInt(1), sqlString("bob"))));
-
-// render page
-
-$db->close();
+$tManager = new TManager();
+$page = isset($_GET["page"]) ? $_GET["page"] : "live";
+$tManager->handlePage($page);
---
+++ src/TManager.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once "Twig/Autoloader.php";
+require_once "Repository/Database.php";
+
+/**
+ * Class TManager - Hauptklasse der TournamentManager Website.
+ */
+class TManager
+{
+  private $db;
+  private $twig;
+
+  public function __construct()
+  {
+    $this->twig = $this->initTwig();
+    $this->db = $this->initDatabase();
+  }
+
+  public function __destruct()
+  {
+    $this->db->close();
+  }
+
+  /**
+   * Erstellt und initialisiert das TWIG-Subsystem und gibt dieses zurück.
+   * @return Twig_Environment
+   */
+  private function initTwig()
+  {
+    Twig_Autoloader::register();
+    $loader = $GLOBALS["TWIG_LOADER"] = new Twig_Loader_Filesystem("templates");
+    $cache = DEV_MODE === "debug" ? false : "cache";
+    $twig = $GLOBALS["TWIG"] = new Twig_Environment($loader, array("cache" => $cache));
+
+    $assetFunc = new Twig_SimpleFunction("asset", function($assetPath)
+    {
+      return DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . $assetPath;
+    });
+
+    $twig->addFunction($assetFunc);
+    return $twig;
+  }
+
+  /**
+   * Erstellt und Initialisiert die Datenbank und gibt diese zurück.
+   * @return Database
+   */
+  private function initDatabase()
+  {
+    $db = $GLOBALS["DB"] = new Database();
+    $db->connect();
+    return $db;
+  }
+
+  /**
+   * Gibt das angegebene Template mit den Parametern aus.
+   * @param $template Das Template, Pfad zu einer .html Datei
+   * @param array $args Argumente die an das Template übergeben werden
+   */
+  public function renderTemplate($template, array $args = array())
+  {
+    echo $this->twig->render($template, $args);
+  }
+
+  /**
+   * Angegeben Seite anzeigen.
+   * @param $pageName Der Name der Seite
+   */
+  public function handlePage($pageName)
+  {
+    switch (strtolower($pageName))
+    {
+      case "live":
+        $this->renderTemplate("live.html");
+        break;
+      case "admin":
+        $this->renderTemplate("admin.html");
+        break;
+      default:
+        $this->renderTemplate("404.html", array("requestedPage" => $pageName));
+    }
+  }
+}
