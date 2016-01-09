fd4609745cd9c1d5cea77541100c6013af5587ae
MegaForge1 <lisophprivat@yahoo.de>
2016-01-09 22:52:09 +0100

    livecontroller hinzugefügt ohne logik

--- src/Controller/LiveController.php
+++ src/Controller/LiveController.php
@@ -1,6 +1,28 @@
 <?php
+require_once "Model/Match.php";
+require_once "Model/Group.php";
+require_once "Model/Player.php";
+require_once "Model/Team.php";
+require_once "Model/Tournament.php";
+require_once "Repository/Database.php";
+require_once "Repository/DatabaseRepository.php";
 
 class LiveController
 {
+    private $db;
+    private $dbRepo;
 
+    public function __construct()
+    {
+        $this->db = $this->initDatabase();
+        $this->dbRepo = new DatabaseRepository($this->db);
+
+        session_start();
+    }
+
+    function GetData()
+    {
+        $arr = array();
+        json_encode($arr);
+    }
 }
