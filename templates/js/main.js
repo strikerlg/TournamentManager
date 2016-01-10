66c6e7b28b5046a426611a82fb84150bdff96d21
Daniel Hauser <daniel.hauser@liwest.at>
2016-01-10 16:07:36 -0500

    DateTime Picker eingebaut; Match-Zeit wird gesynct

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,18 +1,28 @@
 "use strict";
 
-(function() {
+(function($) {
 	$(document).ready(function() {
 		MobileNav.init();
-	});
-})();
 
-var MobileNav = (function() {
+    if ($.fn.datetimepicker) {
+      $(".mf-date").each(function() {
+        $(this).datetimepicker({
+          locale: "de-at",
+          format: "YYYY-MM-DD HH:mm:ss"
+        });
+      });
+    }
+	});
+})(jQuery);
+
+var MobileNav = (function($) {
 	var MOD = {};
 
 	MOD.init = function() {
 		var $mobileNav = $(".mf-mobile-nav");
 		var $pageWrap = $(".mf-page-wrap");
 		var $hamburger = $(".mf-menu .mf-hamburger");
+
 		$hamburger.on("click", function() {
 			$mobileNav.toggleClass("mf-active");
 			$pageWrap.toggleClass("mf-page-toright");
@@ -20,4 +30,15 @@ var MobileNav = (function() {
 	};
 
 	return MOD;
-})();
+})(jQuery);
+
+var TouchMeister = (function($) {
+  var MOD = {};
+
+  var touchStartPos = { x: 0, y: 0 };
+
+  $(document).on("touchstart", function(e) {
+    var touch = e.changedTouches[0];
+    alert(touch.pageX);
+  })
+})(jQuery);
